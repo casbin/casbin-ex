e460d69efffbba7e45e8335d45348eca0bb32a79
ngoclinhng <mail2ngoclinh@gmail.com>
2020-04-01 14:59:59 +0700

    add list_policies test

--- test/enforcer/acl_model_test.exs
+++ test/enforcer/acl_model_test.exs
@@ -1,9 +1,16 @@
 defmodule Acx.Enforcer.AclModelTest do
   use ExUnit.Case, async: true
+  alias Acx.Model.Policy
   alias Acx.Enforcer
 
   @cfile  "../data/acl.conf" |> Path.expand(__DIR__)
   @pfile  "../data/acl.csv" |> Path.expand(__DIR__)
+
+  setup do
+    {:ok, e} = Enforcer.init(@cfile)
+    e = e |> Enforcer.load_policies!(@pfile)
+    {:ok, e: e}
+  end
 
   describe "allow?/2" do
     @test_cases  [
@@ -27,17 +34,130 @@ defmodule Acx.Enforcer.AclModelTest do
       {["peter", "blog_post", "foo"], false},
     ]
 
-    setup do
-      {:ok, e} = Enforcer.init(@cfile)
-      e = e |> Enforcer.load_policies!(@pfile)
-      {:ok, e: e}
-    end
-
     Enum.each(@test_cases, fn {req, res} ->
       test "response #{res} for request #{inspect req}", %{e: e} do
         assert e |> Enforcer.allow?(unquote(req)) === unquote(res)
       end
     end)
+  end
+
+  describe "list_policies/2" do
+    test "returns all policy rules by default", %{e: e} do
+      policies = e |> Enforcer.list_policies()
+      assert [
+        # peter
+
+        %Policy{
+          key: :p,
+          attrs: [sub: "peter", obj: "blog_post", act: "read", eft: "allow"]
+        },
+        %Policy{
+          key: :p,
+          attrs: [sub: "peter", obj: "blog_post", act: "modify", eft: "allow"]
+        },
+        %Policy{
+          key: :p,
+          attrs: [sub: "peter", obj: "blog_post", act: "create", eft: "allow"]
+        },
+
+        # bob
+
+        %Policy{
+          key: :p,
+          attrs: [sub: "bob", obj: "blog_post", act: "read", eft: "allow"]
+        },
+
+        # alice
+
+        %Policy{
+          key: :p,
+          attrs: [sub: "alice", obj: "blog_post", act: "read", eft: "allow"]
+        },
+        %Policy{
+          key: :p,
+          attrs: [sub: "alice", obj: "blog_post", act: "modify", eft: "allow"]
+        },
+        %Policy{
+          key: :p,
+          attrs: [sub: "alice", obj: "blog_post", act: "delete", eft: "allow"]
+        },
+        %Policy{
+          key: :p,
+          attrs: [sub: "alice", obj: "blog_post", act: "create", eft: "allow"]
+        }
+      ] === policies
+    end
+
+    test "returns all policy rules for alice", %{e: e} do
+      policies = e |> Enforcer.list_policies(%{sub: "alice"})
+
+      assert [
+        %Policy{
+          key: :p,
+          attrs: [sub: "alice", obj: "blog_post", act: "read", eft: "allow"]
+        },
+        %Policy{
+          key: :p,
+          attrs: [sub: "alice", obj: "blog_post", act: "modify", eft: "allow"]
+        },
+        %Policy{
+          key: :p,
+          attrs: [sub: "alice", obj: "blog_post", act: "delete", eft: "allow"]
+        },
+        %Policy{
+          key: :p,
+          attrs: [sub: "alice", obj: "blog_post", act: "create", eft: "allow"]
+        }
+      ] === policies
+    end
+
+    test "returns all policy rules for bob", %{e: e} do
+      policies = e |> Enforcer.list_policies(%{sub: "bob"})
+      assert [
+        %Policy{
+          key: :p,
+          attrs: [sub: "bob", obj: "blog_post", act: "read", eft: "allow"]
+        }
+      ] === policies
+    end
+
+    test "returns all policy rules for peter", %{e: e} do
+      policies = e |> Enforcer.list_policies(%{sub: "peter"})
+      assert [
+        %Policy{
+          key: :p,
+          attrs: [sub: "peter", obj: "blog_post", act: "read", eft: "allow"]
+        },
+        %Policy{
+          key: :p,
+          attrs: [sub: "peter", obj: "blog_post", act: "modify", eft: "allow"]
+        },
+        %Policy{
+          key: :p,
+          attrs: [sub: "peter", obj: "blog_post", act: "create", eft: "allow"]
+        }
+      ] === policies
+    end
+
+    test "returns all policy rules where `act` is `create`", %{e: e} do
+      policies = e |> Enforcer.list_policies(%{act: "create"})
+      assert [
+        %Policy{
+          key: :p,
+          attrs: [sub: "peter", obj: "blog_post", act: "create", eft: "allow"]
+        },
+        %Policy{
+          key: :p,
+          attrs: [sub: "alice", obj: "blog_post", act: "create", eft: "allow"]
+        },
+      ] === policies
+    end
+
+    test "returns empty list if no policy rules match given criteria",
+      %{e: e} do
+      assert e |> Enforcer.list_policies(%{sub: "bob", act: "create"}) === []
+    end
+
   end
 
 end
